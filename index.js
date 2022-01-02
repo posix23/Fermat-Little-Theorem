1270e6e36633d5622c8914217b5b557b33ff85bd
Toan Ly <toanly@uw.edu>
2022-01-01 16:48:33 -0800

    update

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-// Check if a number is prime by using Fermat's Little Theorem
-// Any prime number after 2 is odd and therefore, 2 will be used as the base number
-const isPrime = x => {
-  if (x === 2) return true;
+// import modules
+const fs = require('fs');
 
-  if ((x <= 9007199254740991 && x % 2 === 0) || isEven(x)) return false;
-  
-  // Max is 1024
-  if (x < 1025 && Math.pow(2, x - 1) % x === 1) return true;
+// import classes
+const PrimeChecker = require('./lib/PrimeChecker');
 
-  if (largeNumMod(2, x - 1, x) === 1) return true;
-
-  return false;
-};
-
-// This is for checking a very large integer if it is even as mod operator is no longer accurate
-const isEven = x => {
-  x += ""; // Convert x to a string
-
-  if (parseInt(x[x.length - 1]) % 2 === 0) return true;
-
-  return false;
-};
-
-// n^d mod x
-// O(d) run time
-const largeNumMod = (n, d, x) => {
-  let powerMap = new Map(), remainder = Math.pow(n, 2) % x, powerArr = [1, 2];
-
-  powerMap.set(2, remainder);
-
-  for (let i = 4; i <= d; i *= 2) {
-    powerArr.push(i);
-    powerMap.set(i, Math.pow(remainder, 2) % x);
-    remainder = Math.pow(remainder, 2) % x;
-  }
-
-  let curr = d, result = 1;
-  for (let i = powerArr.length - 1; i >= 0; i--) {
-    d -= powerArr[i];
-    if (d >= 0) result *= powerMap.get(powerArr[i]);
-    else d += powerArr[i];
-  }
-
-  return result % x;
-};
+// import txt and json files
+const current = require('./results/currentPrime.json');
 
 const largestPrime = () => {
-  let i = 2;
+  const primeChecker = new PrimeChecker;
+  let x = current.curr;
+
   while (true) {
-    if (isPrime(i)) console.log(i);
-    i++;
+    if (primeChecker.isPrime(x)) {
+      fs.appendFileSync('./results/list_of_known_primes_by_computer.txt', `${x}\n`);
+      fs.writeFileSync('./results/currentPrime.json', `{ "curr": ${x} }`);
+    }
+
+    x++;
   }
 }
 
